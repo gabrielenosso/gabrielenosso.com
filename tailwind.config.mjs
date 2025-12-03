6e1e13bf12c0544ed574dfc8660d37e3045eccd7
Gabriele Nosso <gabrielenosso@xrite.com>
2025-12-03 04:05:35 +0100

    new theme colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,13 @@ export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     darkMode: 'class',
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'night-blue': '#0a1628',
+                'light-blue': '#6b9bd1',
+                'gold': '#d4af37',
+            },
+        },
     },
     plugins: [],
 }
